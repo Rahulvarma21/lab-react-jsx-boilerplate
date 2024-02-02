4faabc2c6ba6fa4204018f4ec12c66793e249375
Rahul <rahul@Rahuls-MacBook-Air.local>
2024-02-02 18:18:25 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,44 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
+const imageData = () => {
   let data = [
     {
-      id:1,
-      img:elephant
+      id: 1,
+      img: elephant,
     },
     {
-      id:2,
-      img:elephant
+      id: 2,
+      img: elephant,
     },
     {
-      id:3,
-      img:elephant
+      id: 3,
+      img: elephant,
     },
     {
-      id:4,
-      img:elephant
-    }
-  ]
+      id: 4,
+      img: elephant,
+    },
+  ];
   return data;
-}
+};
 
 function App() {
   // code here
+  const image = imageData();
+  return (
+    <div>
+      <h1>Kalvium Gallery</h1>
+      <div className="imagesStyle">
+        {image.map((item) => (
+          <div>
+            <img key={item.id} src={item.img} alt={`Image ${item.id}`} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,30 +1,44 @@
 import { Component } from "react";
-import "./App.css"
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
+export default class AppClass extends Component {
+  imageData = () => {
     let data = [
       {
-        id:1,
-        img:elephant
+        id: 1,
+        img: elephant,
       },
       {
-        id:2,
-        img:elephant
+        id: 2,
+        img: elephant,
       },
       {
-        id:3,
-        img:elephant
+        id: 3,
+        img: elephant,
       },
       {
-        id:4,
-        img:elephant
-      }
-    ]
+        id: 4,
+        img: elephant,
+      },
+    ];
     return data;
-  }
+  };
 
   // code here
+  render() {
+    const images = this.imageData();
+    return (
+      <div>
+        <h1>Kalvium Gallery</h1>
+        <div className="imagesStyle">
+          {this.imageData().map((item) => (
+            <div>
+              <img src={item.img} alt={`Image ${item.img}`} />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
